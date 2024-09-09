a860e4e7546a045af4068a0c48622a2b7f36e67d
Luciano Germani <lucianogermani93@gmail.com>
2024-09-09 11:35:08 +0200

    Feat: img hero positioning navbar

--- src/app/[locale]/components/hero/index.jsx
+++ src/app/[locale]/components/hero/index.jsx
@@ -1,9 +1,16 @@
-import './index.scss';
+import { useTranslations } from "next-intl";
+import heroImage from '@/static/media/img/hero.jpg'
+import Image from 'next/image';
 
+import './index.scss';
 const Hero = () => {
+    const t = useTranslations("Hero");
+
     return(
-        <div>
-            Hero section
+        <div className='hero-design'>
+            <div className="img-container">
+                <Image className="img" src={heroImage} alt="drink1"  quality={100} />
+            </div>
         </div>
     )
 }
