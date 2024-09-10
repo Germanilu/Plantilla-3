afadf22549386fbf50f2216703d1892580b0c2dd
Luciano Germani <lucianogermani93@gmail.com>
2024-09-10 16:16:34 +0200

    Fix: specials motion

--- src/app/[locale]/components/specials/index.jsx
+++ src/app/[locale]/components/specials/index.jsx
@@ -2,7 +2,7 @@
 import { useTranslations } from "next-intl";
 import { motion } from "framer-motion";
 import Image from 'next/image';
-import specialImg from '@/static/media/img/specialImg.jpg'
+import specialImg from '@/static/media/img/gnobg.png'
 import './index.scss';
 
 const Specials = () => {
@@ -12,56 +12,146 @@ const Specials = () => {
         <div className="specials-design">
 
             <div className="top-text-container">
-                <div className="text">{t('text')}</div>
-                <div className="title">{t('title')}</div>
-                <div className="additional-text">{t('desc')}</div>
+                <motion.div className="text"
+                 variants={{
+                    hidden: { opacity: 0, y: 50 },
+                    visible: { opacity: 1, y: 0 },
+                  }}
+                  transition={{ delay: 0.2, duration: 0.5 }}
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true }}
+                  >{t('text')}</motion.div>
+                <motion.div className="title"
+                 variants={{
+                    hidden: { opacity: 0, y: 50 },
+                    visible: { opacity: 1, y: 0 },
+                  }}
+                  transition={{ delay: 0.1, duration: 0.5 }}
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true }}
+                  >{t('title')}</motion.div>
+                <motion.div className="additional-text"
+                 variants={{
+                    hidden: { opacity: 0, y: 50 },
+                    visible: { opacity: 1, y: 0 },
+                  }}
+                  transition={{ delay: 0.2, duration: 0.5 }}
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true }}
+                  >{t('desc')}</motion.div>
             </div>
 
             <div className="special-container">
 
                 <div className="special-menu">
 
-                    <div className="drink-container">
+                    <motion.div className="drink-container"
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                      }}
+                      transition={{ delay: 0.1, duration: 0.5 }}
+                      initial="hidden"
+                      whileInView="visible"
+                      viewport={{ once: true }}
+                      >
                         <div className="drink-name">{t('drink-name1')}</div>
                         <span className="drink-price">{t('drink-price1')}</span>
                         <div className="drink-desc">{t('drink-desc1')}</div>
-                    </div>
+                    </motion.div>
 
-                    <div className="drink-container">
+                    <motion.div className="drink-container"
+                     variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                      }}
+                      transition={{ delay: 0.2, duration: 0.5 }}
+                      initial="hidden"
+                      whileInView="visible"
+                      viewport={{ once: true }}
+                      >
                         <div className="drink-name">{t('drink-name2')}</div>
                         <span className="drink-price">{t('drink-price2')}</span>
                         <div className="drink-desc">{t('drink-desc2')}</div>
-                    </div>
+                    </motion.div>
 
-                    <div className="drink-container">
+                    <motion.div className="drink-container"
+                     variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                      }}
+                      transition={{ delay: 0.3, duration: 0.5 }}
+                      initial="hidden"
+                      whileInView="visible"
+                      viewport={{ once: true }}
+                      >
                         <div className="drink-name">{t('drink-name3')}</div>
                         <span className="drink-price">{t('drink-price3')}</span>
                         <div className="drink-desc">{t('drink-desc3')}</div>
-                    </div>
+                    </motion.div>
 
-                    <div className="drink-container">
+                    <motion.div className="drink-container"
+                     variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                      }}
+                      transition={{ delay: 0.4, duration: 0.5 }}
+                      initial="hidden"
+                      whileInView="visible"
+                      viewport={{ once: true }}
+                      >
                         <div className="drink-name">{t('drink-name4')}</div>
                         <span className="drink-price">{t('drink-price4')}</span>
                         <div className="drink-desc">{t('drink-desc4')}</div>
-                    </div>
+                    </motion.div>
 
-                    <div className="drink-container">
+                    <motion.div className="drink-container"
+                     variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                      }}
+                      transition={{ delay: 0.5, duration: 0.5 }}
+                      initial="hidden"
+                      whileInView="visible"
+                      viewport={{ once: true }}>
+
                         <div className="drink-name">{t('drink-name5')}</div>
                         <span className="drink-price">{t('drink-price5')}</span>
                         <div className="drink-desc">{t('drink-desc5')}</div>
-                    </div>
+                    </motion.div>
 
-                    <div className="drink-container">
+                    <motion.div className="drink-container"
+                     variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                      }}
+                      transition={{ delay: 0.6, duration: 0.5 }}
+                      initial="hidden"
+                      whileInView="visible"
+                      viewport={{ once: true }}
+                      >
                         <div className="drink-name">{t('drink-name6')}</div>
                         <span className="drink-price">{t('drink-price6')}</span>
                         <div className="drink-desc">{t('drink-desc6')}</div>
-                    </div>
+                    </motion.div>
 
                 </div>
 
-                <div className="special-img">
+                <motion.div className="special-img"
+                variants={{
+                    hidden: { opacity: 0, x: -200 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                  transition={{ delay: 0.4, duration: 0.5 }}
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true }}
+                  >
                     <Image className="img" src={specialImg} alt="drink" quality={100} />
-                </div>
+                </motion.div>
 
             </div>
 
