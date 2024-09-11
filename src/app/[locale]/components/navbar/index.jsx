d037aec189e9490c84d0e374c6d8f2ca328bb7e8
Luciano Germani <lucianogermani93@gmail.com>
2024-09-11 19:05:02 +0200

    Fix: basic navbar style

--- src/app/[locale]/components/navbar/index.jsx
+++ src/app/[locale]/components/navbar/index.jsx
@@ -1,18 +1,21 @@
 'use client'
 
-import {Link}               from '../../../../navigation';
-import  LanguageSwitcher    from '../language-switcher';
-import { useTranslations }  from "next-intl";
-import { useSelector }      from 'react-redux';
-import { useState }         from 'react';
-import { IoMenu }           from "react-icons/io5";
-import { RxCross2 }         from "react-icons/rx";
+import { Link } from '../../../../navigation';
+import LanguageSwitcher from '../language-switcher';
+import { useTranslations } from "next-intl";
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { IoMenu } from "react-icons/io5";
+import { RxCross2 } from "react-icons/rx";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaGooglePlusSquare } from "react-icons/fa";
 
 import './index.scss';
 import Button from '../button';
 
 export default function Navbar() {
-  const t                       = useTranslations("Navbar");
+  const t = useTranslations("Navbar");
   const [openMenu, setOpenMenu] = useState(false);
   const isMobile = useSelector(state => state.responsive.isMobile);
 
@@ -38,14 +41,14 @@ export default function Navbar() {
                     <Link className="item" href="/contact-us">{t('contact-us')}</Link>
                   </li>
                   <li className="list-item">
-                  <LanguageSwitcher className="language-switcher" />
+                    <LanguageSwitcher className="language-switcher" />
                   </li>
                 </ul>
               }
             </div>
           </nav>
           :
-          
+
           <nav className="navbar">
             <div className="logo">The Lab</div>
             <ul className="navbar-list">
@@ -61,9 +64,16 @@ export default function Navbar() {
               <li className="list-item">
                 <Link className="item" href="/contact-us">{t('contact-us')}</Link>
               </li>
-              <LanguageSwitcher />
+              <div className="language-switcher">
+                <LanguageSwitcher />
+              </div>
             </ul>
-              <Button  href='/blog' text='view-more'/>
+            <div className="socials">
+              <FaSquareFacebook size={23} />
+              <FaSquareInstagram size={23} />
+              <FaGooglePlusSquare size={23} />
+            </div>
+            <Link className="navbar-call-to-action" href="/contact-us">{t('book-a-table')}</Link>
           </nav>
       }
     </>
