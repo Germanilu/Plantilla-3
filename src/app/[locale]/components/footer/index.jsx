b798980a92f4ccbb2e787108147ce0ed0b4fa0ce
Luciano Germani <lucianogermani93@gmail.com>
2024-09-11 18:25:00 +0200

    Feat: Add motion to footer

--- src/app/[locale]/components/footer/index.jsx
+++ src/app/[locale]/components/footer/index.jsx
@@ -1,4 +1,6 @@
+'use client';
 import { useTranslations } from "next-intl";
+import { motion } from "framer-motion";
 import { HiCalendarDays } from "react-icons/hi2";
 import { SiGooglestreetview } from "react-icons/si";
 import { FaPhone } from "react-icons/fa6";
@@ -14,7 +16,16 @@ export default function Footer() {
 
   return (
     <div className="footer-design">
-      <div className="box">
+      <motion.div className="box" 
+        variants={{
+          hidden: { opacity: 0, y: 70 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        transition={{ delay: .5, duration: 0.5 }}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        >
         <div className="logo">{t('logo')}</div>
         <p className="text">{t('text')}</p>
         <div className="hour-box">
@@ -29,17 +40,35 @@ export default function Footer() {
           <FaPhone />
           <div className="hour">{t('phone')}</div>
         </div>
-      </div>
+      </motion.div>
 
-      <div className="box center">
+      <motion.div className="box center"
+       variants={{
+        hidden: { opacity: 0, y: 70 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      transition={{ delay: .5, duration: 0.5 }}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}>
+
         <h3>{t('pages')}</h3>
         <Link href={'/'} className="link">{t('home')}</Link>
         <Link href={'/#contact'} className="link">{t('reservation')}</Link>
         <Link href={'/menu'} className="link">{t('menu')}</Link>
         <Link href={'/contact-us'} className="link">{t('contact')}</Link>
-      </div>
+      </motion.div>
 
-      <div className="box center">
+      <motion.div className="box center"
+       variants={{
+        hidden: { opacity: 0, y: 70 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      transition={{ delay: .5, duration: 0.5 }}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}>
+
         <h3>{t('opening-hours')}</h3>
         <div className="opening-hours-box">
           <div className="days">{t('monday-friday')}</div>
@@ -49,16 +78,25 @@ export default function Footer() {
           <div className="days">{t('saturday-sunday')}</div>
           <div className="hours">{t('hour2')}</div>
         </div>
-      </div>
+      </motion.div>
 
-      <div className="box socials">
+      <motion.div className="box socials"
+       variants={{
+        hidden: { opacity: 0, y: 70 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      transition={{ delay: .5, duration: 0.5 }}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}>
+        
         <h3 className="socials-h3">{t('socials')}</h3>
         <div className="socials-box">
         <FaSquareFacebook size={30}/>
         <FaSquareInstagram size={30}/>
         <FaGooglePlusSquare size={30}/>
         </div>
-      </div>
+      </motion.div>
     </div>
   )
 }
