aa458e049ecf916004a976f79a46ae56838375ba
Luciano Germani <lucianogermani93@gmail.com>
2024-09-12 15:22:09 +0200

    Feat: Add translation about us page

--- src/app/[locale]/about/page.jsx
+++ src/app/[locale]/about/page.jsx
@@ -20,10 +20,10 @@ export default function About() {
           <Image className="img" src={img1} alt="bar neon" quality={100} />
         </div>
         <div className="text-container">
-          <p className="desc">behind the scenes</p>
-          <h2 className="title">our story</h2>
+          <p className="desc">{t('p1')}</p>
+          <h2 className="title">{t('t1')}</h2>
           <p className="text">
-            Nestled in the heart of the city, our cocktail bar is a haven for those who appreciate the art of mixology. Founded by a group of passionate bartenders, our story began with a simple love for crafting unique, memorable experiences one drink at a time. We believe in the magic that happens when the finest spirits meet fresh ingredients and a touch of creativity. From classic cocktails to innovative concoctions, every sip tells a story. Join us and become part of our journey.
+          {t('text1')}
           </p>
         </div>
       </div>
@@ -32,10 +32,10 @@ export default function About() {
           <Image className="img" src={img2} alt="mojito no bg" quality={100} />
         </div>
         <div className="text-container">
-          <p className="desc">crafted with passion</p>
-          <h2 className="title">our cocktails</h2>
+          <p className="desc"> {t('p2')}</p>
+          <h2 className="title"> {t('t2')}</h2>
           <p className="text">
-              At the heart of our bar lies our commitment to crafting cocktails that are as unforgettable as they are delicious. Each drink is a masterpiece, meticulously designed to balance flavors, aromas, and aesthetics. Our menu features a curated selection of classics alongside our signature creations, all made with the finest ingredients sourced from around the world. Whether you’re a fan of timeless favorites or adventurous blends, every cocktail is a journey, carefully mixed to perfection and served with a touch of flair.
+          {t('text2')}
           </p>
         </div>
       </div>
