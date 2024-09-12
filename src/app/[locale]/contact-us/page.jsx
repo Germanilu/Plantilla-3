b93065c37bc8ad996660e4b4ee03b770257bc3bd
Luciano Germani <lucianogermani93@gmail.com>
2024-09-12 12:58:18 +0200

    Feat contact page

--- src/app/[locale]/contact-us/page.jsx
+++ src/app/[locale]/contact-us/page.jsx
@@ -1,14 +1,20 @@
 import { useTranslations }  from "next-intl";
+import Image from 'next/image';
+import contactImg from '@/static/media/img/contact2.jpg'
 import Contact              from '../components/contact/index';
-
 import './page.scss'
 
 export default function ContactUs(){
   const t = useTranslations("Contact-us");
   return (
     <div className="contact-us-design">
+      <div className="img-container">
+        <Image className="img" src={contactImg} alt="bar counter" quality={100} />
+      </div>
+      <div className="contact-container">
       <h1>{t('title')}</h1>
-      <Contact/>
+        <Contact/>
+      </div>
     </div>
   ) 
 }
