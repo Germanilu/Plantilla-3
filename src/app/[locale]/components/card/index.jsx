ecf0ed819b5e8b60650c202b9723a30a3b64ae23
Luciano Germani <lucianogermani93@gmail.com>
2024-09-13 17:08:07 +0200

    Fix: translations and creation menu.js

--- src/app/[locale]/components/card/index.jsx
+++ src/app/[locale]/components/card/index.jsx
@@ -1,90 +1,33 @@
 'use client'
 import { useTranslations } from "next-intl";
 import Image from 'next/image';
-
-import a from '@/static/media/img/drinknobg.png';
-import b from '@/static/media/img/margarita.png';
-import c from '@/static/media/img/h.png';
-import d from '@/static/media/img/m.png';
-import e from '@/static/media/img/a.png';
-import f from '@/static/media/img/r.png';
-import g from '@/static/media/img/n.png';
-import h from '@/static/media/img/b.png';
-import i from '@/static/media/img/j.png';
+import {Cocktails} from '@/static/menu';
 import './index.scss';
 
+
+/**
+ * @component
+ * This Card component will map the cocktails array in static/menu.js and display a card with the information and img of it.
+ */
 const Card = () => {
     const t = useTranslations("Card");
 
     return (
         <div className="card-design">
- 
+            {
+                Cocktails.map((cocktail) => {
+                    return (
+                        <article key={cocktail.id}>
+                            <figure>
+                                <h3 className="title">{cocktail.drinkName}</h3>
+                                <Image className="img" src={cocktail.drinkImg} alt="bar counter" quality={100} />
+                                <p className="text">{t(`${cocktail.drinkDesc}`)}</p>
+                            </figure>
+                        </article>
+                    )
+                })
+            }
 
-
-            <article>
-                <figure>
-                    <h3 className="title">Mojito</h3>
-                    <Image className="img" src={a} alt="bar counter" quality={100} />
-                <p className="text">White rum, fresh mint, and lime juice, lightly sweetened and topped with soda. A crisp, refreshing Cuban classic.</p>
-                </figure>
-            </article>
-           
-            <article>
-                <figure>
-                    <Image className="img" src={b} alt="bar counter" quality={100} />
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis amet cum obcaecati harum eos quod laudantium error sequi similique consectetur?</p>
-                </figure>
-            </article>
-            
-
-            <article>
-                <figure>
-                    <Image className="img" src={c} alt="bar counter" quality={100} />
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis amet cum obcaecati harum eos quod laudantium error sequi similique consectetur?</p>
-                </figure>
-            </article>
-
-            <article>
-                <figure>
-                    <Image className="img" src={d} alt="bar counter" quality={100} />
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis amet cum obcaecati harum eos quod laudantium error sequi similique consectetur?</p>
-                </figure>
-            </article>
-
-            <article>
-                <figure>
-                    <Image className="img" src={e} alt="bar counter" quality={100} />
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis amet cum obcaecati harum eos quod laudantium error sequi similique consectetur?</p>
-                </figure>
-            </article>
-
-            <article>
-                <figure>
-                    <Image className="img" src={f} alt="bar counter" quality={100} />
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis amet cum obcaecati harum eos quod laudantium error sequi similique consectetur?</p>
-                </figure>
-            </article>
-
- 
-
-            <article>
-                <figure>
-                    <Image className="img" src={g} alt="bar counter" quality={100} />
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis amet cum obcaecati harum eos quod laudantium error sequi similique consectetur?</p>
-                </figure>
-            </article>
-            <article>
-                <figure>
-                    <Image className="img" src={h} alt="bar counter" quality={100} />
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis amet cum obcaecati harum eos quod laudantium error sequi similique consectetur?</p>
-                </figure>
-            </article>
-            <article>
-                <figure>
-                    <Image className="img" src={i} alt="bar counter" quality={100} />
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis amet cum obcaecati harum eos quod laudantium error sequi similique consectetur?</p>
-                </figure>
-            </article>
         </div>
     )
 
---
+++ src/static/menu.js
@@ -0,0 +1,66 @@
+import sunsetBreeze from './media/img/a.png';
+import mojito from './media/img/n.png';
+import crimsonBloom from './media/img/r.png';
+import aperolSpritz from './media/img/b.png';
+import lemonMintCooler from './media/img/drinknobg.png';
+import cherryCitrusOldFashioned from './media/img/h.png';
+import caramelVelvet from './media/img/j.png';
+import strawberrySunrise from './media/img/m.png';
+import margarita from './media/img/margarita.png';
+export const Cocktails = [
+    {
+        id:1,
+        drinkName:"Sunset Breeze",
+        drinkDesc:'drink2desc',
+        drinkImg:sunsetBreeze
+    },
+    {
+        id:2,
+        drinkName:"Crimson Bloom",
+        drinkDesc:'drink3desc',
+        drinkImg:crimsonBloom
+    },
+    {
+        id:3,
+        drinkName:"Blue Lagoon Margarita",
+        drinkDesc:'drink4desc',
+        drinkImg:margarita
+    },
+    {
+        id:4,
+        drinkName:"Strawberry Sunrise",
+        drinkDesc:'drink5desc',
+        drinkImg:strawberrySunrise
+    },
+    {
+        id:5,
+        drinkName:"Caramel Velvet",
+        drinkDesc:'drink6desc',
+        drinkImg:caramelVelvet
+    },
+    {
+        id:6,
+        drinkName:"Cherry Citrus Old Fashioned",
+        drinkDesc:'drink7desc',
+        drinkImg:cherryCitrusOldFashioned
+    },
+    {
+        id:7,
+        drinkName:"Classic Aperol Spritz",
+        drinkDesc:'drink9desc',
+        drinkImg:aperolSpritz
+    },
+    {
+        id:8,
+        drinkName:"Mojito",
+        drinkDesc:'drink1desc',
+        drinkImg:mojito
+    },
+    {
+        id:9,
+        drinkName:"Lemon Mint Cooler",
+        drinkDesc:'drink8desc',
+        drinkImg:lemonMintCooler
+    },
+
+]
